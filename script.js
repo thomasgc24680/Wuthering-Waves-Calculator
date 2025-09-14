f327c31a154b6cd9dab182eac00d323f1f24093a
thomasgc124 <thomasgc2313@gmail.com>
2025-09-14 23:54:50 +0900

    캐릭터/무기 선택 화면 수정
    캐릭터/무기 선택 화면 수정
    - 캐릭터/무기 선택 버튼에 속성, 무기 이미지 추가(무기는 무기만 추가)
    - 캐릭터/무기 버튼 분리 생성
    - 이름 레이아웃 수정 : 이름이 두 줄일 시 자동 늘어남, 이름 여백 수정
    - 캐릭터 선택 화면 컨테이너 스크롤 추가

--- script.js
+++ script.js
@@ -129,16 +129,25 @@ function applyFilter(filter) {
 	else if(item.star === 2) starClass = "star-2";
 	else starClass = "star-1";
 	
-    button.innerHTML = `
-      <div class="img-wrapper ${starClass}">
-        <img src="${item.img}" alt="${item.name}">
-      </div>
-      <p>${item.name}</p>
-    `;
-	if (currentTab === "character") {
+	if (currentTab === "character") {	
+		button.innerHTML = `
+		  <div class="img-wrapper ${starClass}">
+			<img src="${item.img}" alt="${item.name}">
+			<img class="attr-icon" src="images/attribute/${item.attribute}.jpg" alt="${item.attribute}">
+			<img class="weap-icon" src="images/filter/weapon/${item.weapon}.jpg" alt="${item.weapon}">
+		  </div>
+		  <p>${item.name}</p>
+		`;
 		button.onclick = () => alert(`${item.name} 선택됨 (캐릭터)`); 
     }
 	else {
+		button.innerHTML = `
+		  <div class="img-wrapper ${starClass}">
+			<img src="${item.img}" alt="${item.name}">
+			<img class="weap-icon" src="images/filter/weapon/${item.weapon}.jpg" alt="${item.weapon}">
+		  </div>
+		  <p>${item.name}</p>
+		`;
       button.onclick = () => alert(`${item.name} 선택됨 (무기)`); 
     }
 	
@@ -149,21 +158,45 @@ function applyFilter(filter) {
 
 ////////////////////////////////
 
+//attribute : glacio(응결), fusion(용융), conducto(전도), aero(기류), spectra(회절), havoc(인멸)
+//weapon : greatsword(대검), straightsword(직검), gun(권총), fist(권갑), amplifier(증폭기)
+
 //캐릭터 데이터
 const characters = [
-		{ "name": "산화", "star": 4, "attribute": "Glacio", "weapon" : "straightsword", "img": "images/Resonator/Sanhua.jpg"},
-		{ "name": "페비", "star": 5, "attribute": "Spectra", "weapon" : "amplifier", "img": "images/Resonator/Phoebe.jpg"},
-		{ "name": "카르티시아", "star": 5, "attribute": "Aero", "weapon" : "straightsword", "img": "images/Resonator/Cartethyia.jpg"},
-		{ "name": "아우구스타", "star": 5, "attribute": "conducto", "weapon" : "greatsword", "img": "images/Resonator/Augusta.jpg"},
-		{ "name": "카를로타", "star": 5, "attribute": "glacio", "weapon" : "gun", "img": "images/Resonator/Carlotta.jpg"},
-		{ "name": "갈브레나", "star": 5, "attribute": "fusion", "weapon" : "gun", "img": "images/Resonator/Galbrena.jpg"},
-		{ "name": "플로로", "star": 5, "attribute": "havoc", "weapon" : "amplifier", "img": "images/Resonator/Phrolova.jpg"},
-		{ "name": "유노", "star": 5, "attribute": "aero", "weapon" : "fist", "img": "images/Resonator/Iuno.jpg"},
+		{ "name": "산화", "star": 4, "attribute": "glacio", "weapon" : "straightsword", "img": "images/resonator/sanhua.jpg"},
+		{ "name": "페비", "star": 5, "attribute": "spectra", "weapon" : "amplifier", "img": "images/resonator/phoebe.jpg"},
+		{ "name": "카르티시아", "star": 5, "attribute": "aero", "weapon" : "straightsword", "img": "images/resonator/cartethyia.jpg"},
+		{ "name": "아우구스타", "star": 5, "attribute": "conducto", "weapon" : "greatsword", "img": "images/resonator/augusta.jpg"},
+		{ "name": "카를로타", "star": 5, "attribute": "glacio", "weapon" : "gun", "img": "images/resonator/carlotta.jpg"},
+		{ "name": "갈브레나", "star": 5, "attribute": "fusion", "weapon" : "gun", "img": "images/resonator/galbrena.jpg"},
+		{ "name": "플로로", "star": 5, "attribute": "havoc", "weapon" : "amplifier", "img": "images/resonator/phrolova.jpg"},
+		{ "name": "유노", "star": 5, "attribute": "aero", "weapon" : "fist", "img": "images/resonator/iuno.jpg"},
+		{ "name": "설지", "star": 4, "attribute": "glacio", "weapon" : "amplifier", "img": "images/resonator/baizhi.jpg"},
+		{ "name": "브렌트", "star": 5, "attribute": "fusion", "weapon" : "straightsword", "img": "images/resonator/brant.jpg"},
+		{ "name": "카카루", "star": 5, "attribute": "conducto", "weapon" : "greatsword", "img": "images/resonator/calcharo.jpg"},
+		{ "name": "장리", "star": 5, "attribute": "fusion", "weapon" : "straightsword", "img": "images/resonator/changli.jpg"},
+		{ "name": "치샤", "star": 4, "attribute": "fusion", "weapon" : "gun", "img": "images/resonator/chixia.jpg"},
+		{ "name": "앙코", "star": 5, "attribute": "fusion", "weapon" : "fist", "img": "images/resonator/encore.jpg"},
+		{ "name": "능양", "star": 5, "attribute": "glacio", "weapon" : "fist", "img": "images/resonator/lingyang.jpg"},
+		{ "name": "루미", "star": 4, "attribute": "conducto", "weapon" : "greatsword", "img": "images/resonator/lumi.jpg"},
+		{ "name": "루파", "star": 5, "attribute": "fusion", "weapon" : "greatsword", "img": "images/resonator/lupa.jpg"},
+		{ "name": "모르테피", "star": 4, "attribute": "fusion", "weapon" : "gun", "img": "images/resonator/mortefi.jpg"},
+		{ "name": "상리요", "star": 5, "attribute": "conducto", "weapon" : "fist", "img": "images/resonator/xiangli Yao.jpg"},
+		{ "name": "음림", "star": 5, "attribute": "conducto", "weapon" : "amplifier", "img": "images/resonator/yinlin.jpg"},
+		{ "name": "유호", "star": 4, "attribute": "glacio", "weapon" : "fist", "img": "images/resonator/youhu.jpg"},
+		{ "name": "연무", "star": 5, "attribute": "conducto", "weapon" : "fist", "img": "images/resonator/yuanwu.jpg"},
+		{ "name": "절지", "star": 5, "attribute": "glacio", "weapon" : "amplifier", "img": "images/resonator/zhezhi.jpg"},
 	];
 	
 //무기 데이터
 const weapons = [
-		{ "name": "천년의 회류", "star": 5, "weaponType" : "straightsword", "img" : "images/weapon/천년의 회류.jpg"},
-		{ "name": "견습용 권총", "star": 1, "weaponType" : "gun", "img" : "images/weapon/견습용 권총.jpg"},
-		{ "name": "수호자의 권총 · 용맹", "star": 3, "weaponType" : "gun", "img" : "images/weapon/수호자의 권총 · 용맹.jpg"}
+		{ "name": "천년의 회류", "star": 5, "weapon" : "straightsword", "img" : "images/weapon/천년의 회류.jpg"},
+		{ "name": "견습용 권총", "star": 1, "weapon" : "gun", "img" : "images/weapon/견습용 권총.jpg"},
+		{ "name": "수호자의 권총 · 용맹", "star": 3, "weapon" : "gun", "img" : "images/weapon/수호자의 권총 · 용맹.jpg"},
+		{ "name": "천년의 회류", "star": 5, "weapon" : "straightsword", "img" : "images/weapon/천년의 회류.jpg"},
+		{ "name": "견습용 권총", "star": 1, "weapon" : "gun", "img" : "images/weapon/견습용 권총.jpg"},
+		{ "name": "수호자의 권총 · 용맹", "star": 3, "weapon" : "gun", "img" : "images/weapon/수호자의 권총 · 용맹.jpg"},
+		{ "name": "천년의 회류", "star": 5, "weapon" : "straightsword", "img" : "images/weapon/천년의 회류.jpg"},
+		{ "name": "견습용 권총", "star": 1, "weapon" : "gun", "img" : "images/weapon/견습용 권총.jpg"},
+		{ "name": "수호자의 권총 · 용맹", "star": 3, "weapon" : "gun", "img" : "images/weapon/수호자의 권총 · 용맹.jpg"},
 	];
