ba7765ba9213c983b350ca0cbbf2023abb2864e5
thomasgc124 <thomasgc2313@gmail.com>
2025-09-16 23:15:24 +0900

    상세 페이지 작업
    상세 페이지 작업
    - 캐릭터 레벨, 돌파 랭크, 스킬 레벨 드롭다운 버튼 추가
    스킬 중요도 캡쳐할 사이트 주소 업로드

--- detail.js
+++ detail.js
@@ -1,3 +1,50 @@
+const skills = ["basic", "skill", "circuit", "liberation", "intro"];
+const skills_KR = ["일반공격", "공명스킬", "공명회로", "공명해방", "변주스킬"];
+const container = document.getElementById("status-container");
+
+skills.forEach((skill, index) => {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("skill");
+
+  //라벨
+  const label = document.createElement("span");
+  label.classList.add("label");
+  label.textContent = skills_KR[index];
+  
+  const skillId = skill.toLowerCase().replace(/\s+/g, "_");
+
+  // 현재 값 select box
+  const selectCurrent = document.createElement("select");
+  selectCurrent.id = `${skillId}_current`;   // 예: 일반공격_current
+  for (let i=1; i<=10; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    selectCurrent.appendChild(option);
+  }
+  
+  // 구분선
+  const divider = document.createElement("span");
+  divider.classList.add("inner-divider");
+
+  // 목표 값 select
+  const selectGoal = document.createElement("select");
+  selectGoal.id = `${skillId}_goal`;   // 예: 일반공격_current
+  for (let i = 1; i <= 10; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    selectGoal.appendChild(option);
+  }
+
+  wrapper.appendChild(label);
+  wrapper.appendChild(selectCurrent);
+  wrapper.appendChild(divider);
+  wrapper.appendChild(selectGoal);
+
+  container.appendChild(wrapper);
+});
+
 // detail.js
 window.onload = function() {
   const params = new URLSearchParams(window.location.search);
