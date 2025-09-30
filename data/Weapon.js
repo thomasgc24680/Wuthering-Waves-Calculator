f271cb7a5e300656763fd9da1d464b2242a9cb0c
thomasgc124 <thomasgc2313@gmail.com>
2025-09-30 13:10:00 +0900

    Delete data directory
    main 브렌치 코드 원복

--- data/Weapon.js
+++
@@ -1,8 +0,0 @@
-//weapon : greatsword(대검), straightsword(직검), gun(권총), fist(권갑), amplifier(증폭기)
-
-//무기
-export const weaponData = [
-	{ "name" : "천년의 회류",			"star" : 5, "weapon" : "straightsword"},
-	{ "name" : "견습용 권총",			"star" : 1, "weapon" : "gun"},
-	{ "name" : "수호자의 권총 · 용맹",	"star" : 3, "weapon" : "gun"},
-];
